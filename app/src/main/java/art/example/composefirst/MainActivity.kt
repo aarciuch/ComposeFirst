ac76f56770f3777d8c934651459fd9e437140a69
artur <artur.arciuch@gmail.com>
2024-09-11 15:14:07 +0200

    2

---
+++ app/src/main/java/art/example/composefirst/MainActivity.kt
@@ -0,0 +1,48 @@
+package art.example.composefirst
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import art.example.composefirst.ui.theme.ComposeFirstTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeFirstTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Cześć $name!",
+        modifier = modifier
+    )
+}
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeFirstTheme {
+        Greeting("Android")
+    }
+}
